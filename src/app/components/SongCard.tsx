385c3ed14bf6267b17ff47b6dcd0029092098ee5
Kumani K <kumanikidd@gmail.com>
2025-03-21 08:42:09 +0000

    component update to be more flexible

--- src/app/components/SongCard.tsx
+++ src/app/components/SongCard.tsx
@@ -2,20 +2,28 @@
 type SongCardProps = {
     img: string;
     alt: string
+    listenLink: string;
+    watchLink?: string;
 }
 
 
-export default function SongCard({img, alt}:SongCardProps) {
+export default function SongCard({img, alt, listenLink, watchLink}:SongCardProps) {
     return (
     <article className="card">
         <img src={img} alt={alt}/>
         <div className="button-wrapper">
             <button className="font-xs">
-                LISTEN NOW
+                <a href={listenLink} target="_blank">
+                    LISTEN NOW
+                </a>
             </button>
-            <button>
-                <img src="./images/watch-now-button.svg"/>
-            </button>
+            {watchLink && 
+                <button>
+                    <a href={watchLink} target="_blank">
+                        <img src="./images/watch-now-button.svg"/>
+                    </a>
+                </button>
+            }
         </div>
     </article>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,11 +24,16 @@ export default function Home() {
           <h2 className="h2">OneTimeAgain</h2>
           <p className="h2 font-xl">OUT NOW</p>
           <div className="button-wrapper">
+
           <button>
-            <img src="./images/listen-now-button.svg" alt="Listen now Button"/>
+            <a href="https://open.spotify.com/artist/6RchwmvR0TrucPeYCqvsdf?flow_ctx=da3b2c7e-2b50-473e-af5a-f48104fdc891%3A1742566682#login" target="_blank">
+              <img src="./images/listen-now-button.svg" alt="Listen now Button"/>
+            </a>
           </button>
           <button>
-            <img src="./images/watch-now-button.svg" alt="Watch the Video Button"/>
+            <a href="https://www.youtube.com/watch?v=LYZvA-f5y5g" target="_blank">
+              <img src="./images/watch-now-button.svg" alt="Watch the Video Button"/>
+            </a>
           </button>
         </div>
       </div>
@@ -39,10 +44,10 @@ export default function Home() {
     <section className="wrapper flow">
       <h2 className="h2 top">MORE MUSIC</h2>
       <div className="grid" data-layout="50-50">
-        <SongCard img="./images/wassupp.png" alt="Wassupp Single Cover"/>
-        <SongCard img="./images/wassupp2.png" alt="Wassupp2 Single Cover"/>
-        <SongCard img="./images/enchante.png" alt="Enchante Single Cover"/>
-        <SongCard img="./images/wild.png" alt="Wild Single Cover"/>
+        <SongCard img="./images/wassupp.png" alt="Wassuppp Single Cover" listenLink="https://open.spotify.com/track/534DSaFxUFjJWnFR4C4yLK"/>
+        <SongCard img="./images/wassupp2.png" alt="Wassuppp2 Single Cover" listenLink="https://open.spotify.com/album/384kwooGht7JdEtF5Yo0ir"/>
+        <SongCard img="./images/enchante.png" alt="Enchante Single Cover" listenLink="https://open.spotify.com/track/36iZVJEDBOuXOD2hCAN6bg"/>
+        <SongCard img="./images/wild.png" alt="Wild Single Cover" listenLink="https://open.spotify.com/track/2yqWBTSKCeDx1yn3cO4kaa" watchLink="https://www.youtube.com/watch?v=bUFUnS49x94"/>
       </div>
     </section>
     </div>
