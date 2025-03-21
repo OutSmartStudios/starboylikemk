ff6f66ec64098aa620b6098b9fd041d4a27b2989
Kumani K <kumanikidd@gmail.com>
2025-03-21 13:37:40 +0000

    font injection to increase speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,31 @@
 import type { Metadata } from "next";
+import bringThaNoizeFont from "next/font/local";
+import { Archivo_Black } from "next/font/google";
 import "./styles/globals.css";
 import { Analytics } from "@vercel/analytics/react"
+
+
+const bringThaNoize = bringThaNoizeFont({
+  src: [
+    {
+      path: "/fonts/bring_tha_noize/Bringthanoize.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/bring_tha_noize/Bringthanoize.woff",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+});
+
+const archivoBlack = Archivo_Black({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 
 export const metadata: Metadata = {
@@ -15,7 +40,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
+      <body className={`${archivoBlack.className} ${bringThaNoize.className}`}>
         {children}
         <Analytics />
       </body>
